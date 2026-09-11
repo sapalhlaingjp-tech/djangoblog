09a0f6d478fe1b904ed8c129ccdc02a5141b0065
sapalhlaingjp-tech <sapalhlaingjp@gmail.com>
2026-09-11 11:28:46 +0900

    css

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    return render(request,"blog/home.html",{"title":"This is the Djangoblog Homepage."})
+
+def about(request):
+    return render(request, "blog/about.html", {"content":"This is the Djangoblog team."})
+
+def contact(request):
+    return render(request, "blog/contact.html", {"title":"Song"})
+
+def base(request):
+    return render(request, "blog/base.html",{"title":"me"})
--- config/settings.py
+++ config/settings.py
@@ -9,10 +9,10 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
+from pathlib import Path
 
 import os
-from dotenv import load_dotenv # type: ignore
-from pathlib import Path
+from dotenv import load_dotenv
 
 load_dotenv()
 
@@ -122,6 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'blog' / 'static' / 'blog',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
